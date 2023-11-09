4d63a31b56eb99b72f3474536b7796040aaa3c1d
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-11-09 14:58:56 +0600

    dashboard server automated

---
+++ start_dashboard.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+ip_var=$(hostname -I)
+ip_var="${ip_var%"${ip_var##*[![:space:]]}"}"
+
+echo "
+
+
+
+
+
+            Dashboard URL: $ip_var:8000
+
+
+
+
+
+"
+
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
+python3 -m http.server &
