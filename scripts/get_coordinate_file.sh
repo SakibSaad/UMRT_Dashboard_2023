b0c08a554f078c4a27d3070cdf1d3cdf15594c73
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-12-21 22:47:44 +0600

    getting remote file |tested

--- scripts/get_coordinate_file.sh
+++ scripts/get_coordinate_file.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/expect -f
 
 # Set variables
-set user "neo"
-set url "127.0.0.1"
+set user "nvidia"
+set url "192.168.1.120"
 set remoteFile "$user@$url:/home/neo/.GPS_Coordinate_Loader_GUI/gps_data.json"
 set localDestination "./"
-set password "meaw"
+set password "jetsonmini"
 
 # Run scp and pass password
 spawn scp $remoteFile $localDestination
