a3130765d35c0d810736981b2778a46fdb5881a7
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-12-23 12:32:11 +0600

    Dynamic MAP loading (Dashboard loads map based on coordinate) |tested

---
+++ start_dashboard_no_gps_data_file_pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# server setup
+ip_var=$(hostname -I | awk '{print $1}')
+rm -rf nohup.out
+nohup ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
+nohup python3 -m http.server &
+
+echo "
+
+
+
+
+	Dashboard URL: $ip_var:8000
+
+
+
+
+"
