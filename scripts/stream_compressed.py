5579802e539d8da2ee7e850097f66ff3773a0eed
SakibSaad <sakibsaad9988@gmail.com>
2023-12-30 14:41:07 -0500

    video added

--- scripts/stream_compressed.py
+++ scripts/stream_compressed.py
@@ -4,20 +4,16 @@ import rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage  # Import CompressedImage message
 
 class WebcamStreamNode(Node):
     def __init__(self):
         super().__init__('webcam_stream_node')
-        self.publisher_ = self.create_publisher(Image, 'webcam_image', 10)
+        self.publisher_ = self.create_publisher(CompressedImage, 'webcam_image/compressed', 10)  # Change topic name
         # Increase the timer frequency to increase fps
         self.timer = self.create_timer(0.05, self.publish_webcam_image)
         self.cap = cv2.VideoCapture(0)  # 0 represents the default camera (you may need to change this depending on your setup)
         self.bridge = CvBridge()
-
-        # Set up video writer with MPEG-2 compression
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.out = cv2.VideoWriter('webcam_output.avi', fourcc, 20.0, (640, 480))
 
     def publish_webcam_image(self):
         ret, frame = self.cap.read()
@@ -25,11 +21,16 @@ class WebcamStreamNode(Node):
             # Resize the frame to lower the resolution
             frame = cv2.resize(frame, (320, 240))
 
-            # Encode frame using OpenCV VideoWriter
-            self.out.write(frame)
+            # Compress the frame
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]  # Adjust quality as needed
+            _, compressed_data = cv2.imencode('.jpg', frame, encode_param)
 
-            # Convert and publish the frame as ROS Image message
-            msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+            # Convert compressed data to bytes and create CompressedImage message
+            msg = CompressedImage()
+            msg.format = 'jpeg'
+            msg.data = compressed_data.tobytes()
+
+            # Publish the CompressedImage message
             self.publisher_.publish(msg)
 
 def main(args=None):
@@ -37,8 +38,7 @@ def main(args=None):
     node = WebcamStreamNode()
     rclpy.spin(node)
 
-    # Release the video writer and capture objects
-    node.out.release()
+    # Release the capture object
     node.cap.release()
 
     node.destroy_node()
@@ -46,4 +46,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
